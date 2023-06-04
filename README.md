bcbeb49b39c2849ae744f4e7cd90911649c9b49e
iezx <101031288+iezx@users.noreply.github.com>
2023-06-04 16:07:38 +0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,21 @@
+# Super Whois
+域名、IP WHOIS 搜索系统
+
+# 使用方法：
+1.在下方输入框中输入要查询的域名或 IP 地址，然后选择查询类型。  
+2.点击提交按钮进行查询。  
+3.查询结果将会显示在下方。  
+![1685865839660.png](https://cdn.807070.xyz/img/new/2023/06/04/647c457240a57.png)
+
+
+    
+# Super Whois
+Domain name, IP WHOIS search system
+
+# Method of use: 
+1. Enter the domain name or IP address to be queried in the input box below, and then select the query type.  
+2. Click on the Enquiry button.
+3. The result will be displayed below.  
+![1685865839660.png](https://cdn.807070.xyz/img/new/2023/06/04/647c457240a57.png)
+
+
---
+++ index.php
@@ -0,0 +1,137 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Domain Name, IP WHOIS Search</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="description" content="Welcome to Domain Name, IP WHOIS Search. You can check domain whois and ipv4 ipv6 whois." />
+    <link rel="shortcut icon" href="https://cdn.807070.xyz/img/new/2023/01/14/63c2a68d3bb10.png">
+</head>
+<body>
+    <h1>Domain Name, IP WHOIS Search System</h1>
+    <form method="post" action="">
+        <input type="text" name="query" placeholder="Please enter!" required>
+        <select name="type">
+            <option value="whois" <?php if($_POST['type'] == 'whois') echo 'selected'; ?>>Domain WHOIS Search</option>
+            <option value="ipwhois" <?php if($_POST['type'] == 'ipwhois') echo 'selected'; ?>>IP WHOIS Search</option>
+            <option value="ns" <?php if($_POST['type'] == 'ns') echo 'selected'; ?>>Domain NS Search</option>
+        </select>
+        <input type="submit" name="submit" value="Enquiry">
+    </form>
+
+    <?php
+    error_reporting(0); // 禁用错误报告
+
+    if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['submit'])) {
+        $query = $_POST['query'];
+        $type = $_POST['type'];
+
+        switch ($type) {
+            case 'whois':
+                performWhoisQuery($query);
+                break;
+            case 'ipwhois':
+                performIPWhoisQuery($query);
+                break;
+            case 'ns':
+                performNSQuery($query);
+                break;
+        }
+    }
+
+    function performWhoisQuery($domain) {
+        require_once __DIR__ . '/whois_servers.php';
+        $extension = getDomainExtension($domain);
+
+        if (isset($whoisServers[$extension])) {
+            $server = $whoisServers[$extension];
+            $result = queryWhoisServer($server, $domain);
+
+            echo '<div class="result">';
+            if (!empty($result)) {
+                echo '<h2>' . $domain . ' WHOIS information</h2>';
+                echo '<p>Searched from: ' . $server . '</p>';
+                echo '<ul>';
+                foreach ($result as $line) {
+                    echo '<li>' . $line . '</li>';
+                }
+                echo '</ul>';
+            } else {
+                echo '<h2>Unable to find WHOiS information of the domain name: The domain name may not be registered or the server is not searched.</h2>';
+            }
+            echo '</div>';
+        } else {
+            echo '<div class="result">';
+            echo '<h2>WHOIS search for this domain name is not supported.</h2>';
+            echo '</div>';
+        }
+    }
+
+    function performIPWhoisQuery($ip) {
+        $server = 'whois.apnic.net';
+        $result = queryWhoisServer($server, $ip);
+
+        echo '<div class="result">';
+        if (!empty($result)) {
+            echo '<h2>' . $ip . ' WHOIS information</h2>';
+            echo '<p>Searched from: ' . $server . '</p>';
+            echo '<ul>';
+            foreach ($result as $line) {
+                echo '<li>' . $line . '</li>';
+            }
+            echo '</ul>';
+        } else {
+            echo '<h2>IP WHOIS information not available.</h2>';
+        }
+        echo '</div>';
+    }
+
+    function performNSQuery($domain) {
+        $result = getNSRecords($domain);
+
+        echo '<div class="result">';
+        if (!empty($result)) {
+            echo '<h2>' . $domain . ' NS information</h2>';
+            echo '<ul>';
+            foreach ($result as $record) {
+                echo '<li>' . $record . '</li>';
+            }
+            echo '</ul>';
+        } else {
+            echo '<h2>NS information not available: The domain name may not be registered or the server may not be searched.</h2>';
+        }
+        echo '</div>';
+    }
+
+    function getDomainExtension($domain) {
+        $parts = explode('.', $domain);
+        $extension = end($parts);
+        return strtolower($extension);
+    }
+
+    function queryWhoisServer($server, $query) {
+        $result = array();
+        $fp = @fsockopen($server, 43, $errno, $errstr, 10);
+        if ($fp) {
+            fputs($fp, $query . "\r\n");
+            while (!feof($fp)) {
+                $result[] = fgets($fp);
+            }
+            fclose($fp);
+        }
+        return $result;
+    }
+
+    function getNSRecords($domain) {
+        $result = array();
+        $output = @dns_get_record($domain, DNS_NS);
+        if (!empty($output)) {
+            foreach ($output as $record) {
+                $result[] = $record['target'];
+            }
+        }
+        return $result;
+    }
+    ?>
+</body>
+</html>
