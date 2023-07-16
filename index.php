e5febfcdfd82944ad4aca5c39d8d181c9c751e12
iezx <101031288+iezx@users.noreply.github.com>
2023-07-16 18:04:38 +0800

    Add files via upload

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
     <title>Super Whois</title>
     <link rel="stylesheet" type="text/css" href="style.css">
@@ -11,70 +11,82 @@
 <body>
     <h1>Domain Name, IP WHOIS Search System</h1>
     <form method="post" action="">
-    <input type="text" name="query" placeholder="Please enter and press Enquiry." required>
+        <input type="text" name="query" placeholder="Please enter and press Enquiry." required value="<?php echo isset($_POST['query']) ? removeSpaces($_POST['query']) : ''; ?>">
         <select name="type">
-            <option value="whois" <?php if($_POST['type'] == 'whois') echo 'selected'; ?>>Domain WHOIS Search</option>
-            <option value="ipwhois" <?php if($_POST['type'] == 'ipwhois') echo 'selected'; ?>>IP WHOIS Search</option>
-            <option value="ns" <?php if($_POST['type'] == 'ns') echo 'selected'; ?>>Domain NS Search</option>
+        <?php
+        $options = [
+            'whois' => 'Domain WHOIS Search',
+            'ipwhois' => 'IP WHOIS Search',
+            'ns' => 'Domain NS Search'
+        ];
+
+        foreach ($options as $value => $label) {
+            $selected = (isset($_POST['type']) && $_POST['type'] === $value) ? 'selected' : '';
+            echo '<option value="' . $value . '" ' . $selected . '>' . $label . '</option>';
+        }
+        ?>
         </select>
         <input type="submit" name="submit" value="Enquiry">
     </form>
     
     <?php
-    //V1.0.1 支持空格判断，在提交表单之前自动去除输入中的空格
-    //v1.0.2 更新空格判断代码
-    //v1.0.3 支持IDN域名查询，输出Whois服务器
-    //v1.0.4 CSS代码更新和优化
+    //v1.0.5  1.优化搜索代码 2.更新空格判断代码
 
     //error_reporting(0); // 禁用错误报告代码
 
-    // 去除输入域名中的空格
+    // 去除输入域名中的所有空格
+    function removeSpaces($input) {
+        return preg_replace('/\s+/', '', $input);
+    }
+
     if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['submit'])) {
-    $query = $_POST['query'];
-    $type = $_POST['type'];
+        $query = removeSpaces($_POST['query']);
+        $type = $_POST['type'];
 
-    $query = idn_to_ascii($query);
+        $query = idn_to_ascii($query);
 
-    switch ($type) {
-        case 'whois':
-            performWhoisQuery($query);
-            break;
-        case 'ipwhois':
-            performIPWhoisQuery($query);
-            break;
-        case 'ns':
-            performNSQuery($query);
-            break;
-    }
-}
-// 执行 WHOIS 查询
-    function performWhoisQuery($domain) {
-    require_once __DIR__ . '/whois_servers.php';
-    $extension = getDomainExtension($domain);
-
-    if (isset($whoisServers[$extension])) {
-        $server = $whoisServers[$extension];
-        $result = queryWhoisServer($server, $domain);
-
-        echo '<div class="result">';
-        if (!empty($result)) {
-            echo '<h2>' . $domain . ' WHOIS Information</h2>';
-            echo '<p>Searched from: ' . $server . '</p>'; // 输出Whois服务器
-            echo '<ul>';
-            foreach ($result as $line) {
-                echo '<li>' . $line . '</li>';
-            }
-            echo '</ul>';
-        } else {
-            echo '<h2>Unable to find WHOIS information for the domain name. The domain name may not be registered or the server is not accessible.</h2>';
+        switch ($type) {
+            case 'whois':
+                performWhoisQuery($query);
+                break;
+            case 'ipwhois':
+                performIPWhoisQuery($query);
+                break;
+            case 'ns':
+                performNSQuery($query);
+                break;
         }
-        echo '</div>';
-    } else {
-        echo '<div class="result">';
-        echo '<h2>WHOIS search for this domain name is not supported.</h2>';
-        echo '</div>';
     }
-}
+
+    // 执行 WHOIS 查询
+    function performWhoisQuery($domain) {
+        require_once __DIR__ . '/whois_servers.php';
+        $extension = getDomainExtension($domain);
+
+        if (isset($whoisServers[$extension])) {
+            $server = $whoisServers[$extension];
+            $result = queryWhoisServer($server, $domain);
+
+            echo '<div class="result">';
+            if (!empty($result)) {
+                echo '<h2>' . $domain . ' WHOIS Information</h2>';
+                echo '<p>Searched from: ' . $server . '</p>'; // 输出Whois服务器
+                echo '<ul>';
+                foreach ($result as $line) {
+                    echo '<li>' . $line . '</li>';
+                }
+                echo '</ul>';
+            } else {
+                echo '<h2>Unable to find WHOIS information for the domain name. The domain name may not be registered or the server is not accessible.</h2>';
+            }
+            echo '</div>';
+        } else {
+            echo '<div class="result">';
+            echo '<h2>WHOIS search for this domain name is not supported.</h2>';
+            echo '</div>';
+        }
+    }
+
     // 执行 IP WHOIS 查询
     function performIPWhoisQuery($ip) {
         $server = 'whois.apnic.net';
@@ -94,6 +106,7 @@
         }
         echo '</div>';
     }
+
     // 执行 NS 查询
     function performNSQuery($domain) { 
         $result = getNSRecords($domain);
@@ -111,7 +124,7 @@
         }
         echo '</div>';
     }
-    
+
     function getNSRecords($domain) {
         $result = array();
         $output = @dns_get_record($domain, DNS_NS);
@@ -122,13 +135,14 @@
         }
         return $result;
     }
-    
+
     // 获取域名后缀
     function getDomainExtension($domain) {
         $parts = explode('.', $domain);
         $extension = end($parts);
         return strtolower($extension);
     }
+
     // 查询 WHOIS 服务器
     function queryWhoisServer($server, $query) {
         $result = array();
@@ -142,10 +156,9 @@
         }
         return $result;
     }
-
     ?>
 </body>
 <footer>
-    <p><a href="https://github.com/iezx/Super-Whois" target="_blank">Super Whois</a> Version 1.0.4</p>
+    <p><a href="https://github.com/iezx/Super-Whois" target="_blank">Super Whois</a> Version 1.0.5</p>
 </footer>
 </html>
