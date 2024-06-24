5605ce27608dbe0ab659fd9e30da211194eb0ecc
iezx <101031288+iezx@users.noreply.github.com>
2024-06-24 20:51:23 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,23 +1,34 @@
 # Super Whois
-域名、IP WHOIS 搜索系统  
+域名、IP&ASN WHOIS 查询系统  
 支持IDN域名
 
 # 使用方法：
-1. 在下方输入框中输入要查询的域名或 IP 地址，然后选择查询类型。  
+1. 首先选择查询类型"域名 Whois 搜索"或"IP&ASN Whois 搜索"，然后在输入框中输入要查询的域名、IP地址或ASN。
 2. 点击提交按钮进行查询。  
-3. 查询结果将会显示在下方。  
-
+3. 查询结果将会显示在下方。
+4. 部分 Whois 查询结果会显示服务器 IP，请在 "return str_replace"中的 XXX 位置修改服务器 IP，以保护您的服务器 IP 信息。如果查询结果中有服务器 IP 地址，搜索结果中IP地址将会替换成"Super Whois IP Privacy Function"。
+```
+            $result = array_map(function ($line) {
+                return str_replace('XXX', 'Super Whois IP Privacy Function', $line);
+            }, $result);
+```
 搜索外观  
 ![1692954131176.png](https://cdn.807070.xyz/img/new/2023/08/25/1e1sTe8C1W.png)
     
 # Super Whois
-Domain name, IP WHOIS search system  
+Domain Name, IP&ASN WHOIS Search System  
 Support IDN domain name
 
 # Method of use: 
-1. Enter the domain name or IP address to be queried in the input box below, and then select the query type.  
+1. First, select the query type "Domain Whois Search" or "IP&ASN Whois Search", and then enter the domain name, IP address or ASN to be queried in the input box.
 2. Click on the Enquiry button.
-3. The result will be displayed below.  
+3. The result will be displayed below.
+4. Some Whois query results will show server IP, please change the server IP in the XXX position in "return str_replace" to protect your server IP information. If there is a server IP address in the query result, the IP address in the search result will be replaced with "Super Whois IP Privacy Function".
+```
+            $result = array_map(function ($line) {
+                return str_replace('XXX', 'Super Whois IP Privacy Function', $line);
+            }, $result);
+```
 
 Search Appearance  
 ![1692954131176.png](https://cdn.807070.xyz/img/new/2023/08/25/1e1sTe8C1W.png)
--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <title>Super Whois</title>
     <link rel="stylesheet" type="text/css" href="style.css">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="description" content="Welcome to Domain Name, IP&ASN WHOIS Search. You can check domain whois and ipv4 ipv6 whois." />
+    <meta name="description" content="Welcome to use domain name, IP&ASN WHOIS search system to query domain name information. You can check domain whois, ipv4, ipv6 and ASN whois information." />
     <link rel="shortcut icon" href="https://cdn.807070.xyz/img/new/2023/01/14/63c2a68d3bb10.png">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -32,9 +32,61 @@
         <input type="submit" name="submit" value="Enquiry">
     </form>
 
-    <?php
-    // v1.1.0 1.优化CSS 2.更新和优化代码 2.1.优化判断域名是否已注册 2.2.执行 WHOIS 查询部分代码 3.添加支持ASN查询
+    <div class="illustrate" id="englishGuide">
 
+        <h4><a href="#" onclick="toggleLanguage('chinese')">中文说明</a></h4>
+        <h3>Welcome to the Domain Name, IP&amp;ASN WHOIS Search System!</h3>
+        <p>You can use this tool to query domain name information, IP addresses, and ASN information. Simply enter the domain name, IP address, or ASN number in the input field above and select the appropriate search type (Domain WHOIS Search or IP&amp;ASN WHOIS Search).</p>
+
+        <h2>How to Use:</h2>
+        <ol>
+            <li>Enter the domain name, IP address, or ASN number in the input field.</li>
+            <li>Select the appropriate search type (Domain or IP&amp;ASN WHOIS Search).</li>
+            <li>Click the "Enquiry" button to get the results.</li>
+            <li>After submitting your query, you will see detailed WHOIS information for the specified domain name, IP address, or ASN number.</li>
+        </ol>
+
+        <h2>Results:</h2>
+        <p>The results include registration details, expiration date, registrar information, DNS servers, and more.</p>
+        <h2>Domain Examples:</h2>
+        <p>Domain Name (Example: example.com)</p>
+        <p>IDN Domain Name (Example: 你好.世界)</p>
+        <h2>ASN Examples:</h2>
+        <p>ASN 16bit (Example: AS15169)</p>
+        <p>ASN 32bit (Example: AS401308)</p>
+        <h2>IP Examples:</h2>
+        <p>IPv4 (Example: 8.8.8.8)</p>
+        <p>IPv6 (Example: 2400:3200::1)</p>
+        <h4>Searching ASN/IPv4/IPv6 may take longer time, if timeout, please retry.</h4>
+    </div>
+
+    <div class="illustrate" id="chineseGuide" style="display: none;">
+        <h4><a href="#" onclick="toggleLanguage('english')">English</a></h4>
+        <h3>欢迎使用域名、IP和ASN WHOIS查询系统！</h3>
+        <p>您可以使用此工具查询域名信息、IP地址和ASN信息。只需在上方的输入框中输入域名、IP地址或ASN号码，并选择适当的搜索类型（域名WHOIS搜索或IP&ASN WHOIS搜索）。</p>
+        <h2>使用方法：</h2>
+        <ol>
+            <li>在输入框中输入域名、IP地址或ASN号码。</li>
+            <li>选择适当的搜索类型（域名或IP&ASN WHOIS搜索）。</li>
+            <li>点击"查询"按钮以获取结果。</li>
+            <li>提交查询后，您将看到指定域名、IP地址或ASN号码的详细WHOIS信息。</li>
+        </ol>
+        <h2>域名示例：</h2>
+        <p>域名（示例：example.com）</p>
+        <p>IDN 域名（示例：你好.世界）</p>
+        <h2>ASN 示例：</h2>
+        <p>ASN 16bit（示例：AS15169）</p>
+        <p>ASN 32bit（示例：AS401308）</p>
+        <h2>IP 示例：</h2>
+        <p>IPv4（示例：8.8.8.8）</p>
+        <p>IPv6（示例：2400:3200::1）</p>
+        <h4>搜索 ASN/IPv4/IPv6 可能需要花费更长时间，如果超时，请重试。</h4>
+    </div>
+
+
+
+    <?php
+    // v1.1.1 1.替换ip地址 (在return str_replace输入服务器IP) 部分whois会显示服务器IP 2.使用正则表达式来提取主要信息 3.增加使用说明 4.优化获取输出主要信息的代码
     // error_reporting(0); // 禁用错误报告代码 
 
     // 去除输入域名中的所有空格
@@ -58,7 +110,11 @@
                 break;
         }
     }
+    if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['submit'])) {
 
+        // 隐藏illustrate区域
+        echo '<style>.illustrate { display: none; }</style>';
+    }
     // 执行 WHOIS 查询
     function performWhoisQuery($domain)
     {
@@ -69,8 +125,15 @@
             $server = $whoisServers[$extension];
             $result = queryWhoisServer($server, $domain);
 
+            // 替换ip地址为IP privacy service provided by Super Whois 在XXX输入你服务器IP Enter your server IP in XXX
+            $result = array_map(function ($line) {
+                return str_replace('XXX', 'Super Whois IP Privacy Function', $line);
+            }, $result);
+
             $isRegistered = isDomainRegistered($result);
             $currentDate = date('Y-m-d'); // 获取当前日期
+            $outputtedInfo = []; // 存储已输出的信息
+
             // 追踪是否已输出注册商信息
             $hasRegistrarInfo = false;
             echo '<div class="result">';
@@ -83,54 +146,55 @@
                 echo '<p>Current Date: ' . $currentDate . '</p>';
 
                 // 获取并输出主要信息
+                $whoisDetails = [
+                    'Expiration Date' => 'Expiration Date',
+                    'Registrar Registration Expiration Date' => 'Expiration Date',
+                    'Registry Expiry Date' => 'Expiration Date',
+                    'Expiry Date:' => 'Expiration Date',
+                    'Expiration Time:' => 'Expiration Date',
+                    'expires' => 'Expiration Date',
+                    'Creation Date' => 'Registration Date',
+                    'Domain Registration Date' => 'Registration Date',
+                    'created' => 'Registration Date',
+                    'Registration Time:' => 'Registration Date',
+                    'Updated Date' => 'Last Updated Date',
+                    'Last Updated' => 'Last Updated Date',
+                    'last-update:' => 'Last Updated Date',
+                    'modified' => 'Last Updated Date',
+                    'Name Server' => 'DNS',
+                    'Name Servers' => 'DNS',
+                    'NS' => 'DNS',
+                    'nserver:' => 'DNS',
+                    'DNSSEC' => 'DNSSEC',
+                    'Registrant:' => 'Registrant',
+                    'Registrant Name:' => 'Registrant',
+                    'Registrant Organization:' => 'Registrant',
+                    'Registrant Email' => 'Registrant Email',
+                    'Registrar:' => 'Registrar',
+                    'Registrar WHOIS Server' => 'Registrar',
+                    'registrar:' => 'Registrar',
+                    'Status' => 'Domain Status',
+                    'Domain Status' => 'Domain Status',
+                    'DNSSEC:' => 'DNSSEC',
+
+                ];
+
                 foreach ($result as $line) {
-                    // 判断是否包含到期日期信息
-                    if (strpos($line, 'Expiration Date') !== false || strpos($line, 'Registrar Registration Expiration Date') !== false || strpos($line, 'Registry Expiry Date') !== false || strpos($line, 'Expiry Date:') !== false || strpos($line, 'Expiration Time:') !== false) {
-                        // 输出到期日期
-                        echo '<p>Expiration Date: ' . trim(str_replace(['Expiration Date:', 'Registrar Registration Expiration Date:', 'Registry Expiry Date:', 'Expiry Date:', 'Expiration Time:'], '', $line)) . '</p>';
-                    }
-                    // 判断是否包含注册日期信息
-                    elseif (strpos($line, 'Creation Date') !== false || strpos($line, 'Domain Registration Date') !== false || strpos($line, 'created:') !== false || strpos($line, 'Registration Time:') !== false) {
-                        // 输出注册日期
-                        echo '<p>Registration Date: ' . trim(str_replace(['Creation Date:', 'Domain Registration Date:', 'created:', 'Registration Time:'], '', $line)) . '</p>';
-                    }
-                    // 判断是否包含更新日期信息
-                    elseif (strpos($line, 'Updated Date') !== false || strpos($line, 'Last Updated') !== false || strpos($line, 'last-update:') !== false) {
-                        // 输出更新日期
-                        echo '<p>Last Updated Date: ' . trim(str_replace(['Updated Date:', 'Last Updated:', 'last-update:'], '', $line)) . '</p>';
-                    }
-                    // 判断是否包含 NS 服务器信息
-                    elseif (strpos($line, 'Name Server') !== false || strpos($line, 'Name Servers') !== false || strpos($line, 'NS') !== false || strpos($line, 'nserver:') !== false) {
-                        // 输出 NS 服务器信息
-                        echo '<p>DNS: ' . trim(str_replace(['Name Server:', 'Name Servers:', 'NS:', 'nserver:'], '', $line)) . '</p>';
-                    }
-
-                    // 判断是否包含注册人信息
-                    elseif (strpos($line, 'Registrant:') !== false || strpos($line, 'Registrant Name:') !== false || strpos($line, 'Registrant Organization:') !== false) {
-                        // 输出注册人信息
-                        echo '<p>Registrant: ' . trim(str_replace(['Registrant:', 'Registrant Name:', 'Registrant Organization:'], '', $line)) . '</p>';
-                    }
-                    // 判断是否包含注册人邮箱
-                    elseif (strpos($line, 'Registrant Email') !== false) {
-                        // 输出注册人邮箱
-                        echo '<p>Registrant Email: ' . trim(str_replace('Registrant Email:', '', $line)) . '</p>';
-                    }
-
-                    // 判断是否包含注册商信息
-                    elseif (!$hasRegistrarInfo && (strpos($line, 'Registrar:') !== false || strpos($line, 'Registrar WHOIS Server') !== false || strpos($line, 'registrar:') !== false)) {
-                        // 输出注册商信息
-                        echo '<p>Registrar: ' . trim(str_replace(['Registrar:', 'Registrar WHOIS Server:', 'registrar:'], '', $line)) . '</p>';
-
-                        // 将 $hasRegistrarInfo 设置为 true，确保只输出一次注册商信息
-                        $hasRegistrarInfo = true;
-                    }
-                    // 判断是否包含域名状态信息
-                    elseif (strpos($line, 'Status:') !== false) {
-                        // 输出域名状态信息
-                        echo '<p>Status: ' . trim(str_replace('Status:', '', $line)) . '</p>';
+                    foreach ($whoisDetails as $keyword => $infoType) {
+                        // 使用精确匹配避免误识别
+                        if (preg_match('/^\s*' . preg_quote($keyword, '/') . '\s*(.*)$/i', $line, $matches)) {
+                            $value = trim($matches[1]);
+                            if (!isset($outputtedInfo[$infoType])) {
+                                $outputtedInfo[$infoType] = [];
+                            }
+                            if (!in_array($value, $outputtedInfo[$infoType])) {
+                                echo '<p>' . $infoType . ': ' . ltrim($value, ': ') . '</p>';
+                                $outputtedInfo[$infoType][] = $value; // 添加到已输出列表
+                            }
+                            break;
+                        }
                     }
                 }
-
 
                 echo '</div>';
                 echo '<h3 class="details-toggle" onclick="toggleDetails()">Show Details</h3>';
@@ -247,7 +311,7 @@
     ?>
 </body>
 <footer>
-    <p><a href="https://github.com/iezx/Super-Whois" target="_blank">Super Whois</a> Version 1.1.0 </p>        
+    <p><a href="https://github.com/iezx/Super-Whois" target="_blank">Super Whois</a>Version 1.1.1</p>
 </footer>
 <script>
     function toggleDetails() {
@@ -263,4 +327,16 @@
         }
     }
 </script>
+<script>
+    function toggleLanguage(language) {
+        if (language === 'english') {
+            document.getElementById('englishGuide').style.display = 'block';
+            document.getElementById('chineseGuide').style.display = 'none';
+        } else if (language === 'chinese') {
+            document.getElementById('englishGuide').style.display = 'none';
+            document.getElementById('chineseGuide').style.display = 'block';
+        }
+    }
+</script>
+
 </html>
